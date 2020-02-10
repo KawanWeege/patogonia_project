b63e4f66764fcc2556d306e8bd3747b83f189cd6
DragonOfWar <47753585+KawanWeege@users.noreply.github.com>
2020-02-09 22:46:17 -0300

    init

---
+++ scripts/player.gd
@@ -0,0 +1,34 @@
+extends KinematicBody2D
+
+export(float) var velocidade_maxima: float
+export(float) var aceleracao: float
+export(float) var gravidade: float
+
+var _velocidade: Vector2
+
+func _physics_process(delta: float) -> void:
+	# Pegar os inputs do jogador
+	var direcao: Vector2 = Vector2.ZERO
+	
+	if Input.is_action_pressed("andar_para_direita"):
+		direcao.x += 1
+	if Input.is_action_pressed("andar_para_esquerda"):
+		direcao.x -= 1
+	
+	# Pegar a velocidade, e descartar o y. 
+	# Vamos primeiro calcular a velocidade horizontal e dps adicionar gravidade
+	var lvdir = _velocidade
+	lvdir.y = 0
+	
+	var velocidade_alvo = direcao.normalized() * velocidade_maxima
+	var accel = -aceleracao
+	
+	if(lvdir.dot(_velocidade) > 0):
+		accel = aceleracao
+	
+	lvdir = lvdir.linear_interpolate (velocidade_alvo, accel * delta)
+	
+	_velocidade.x = lvdir.x
+	_velocidade.y += gravidade * delta
+	
+	_velocidade = move_and_slide(_velocidade, Vector2.UP)
